4f7898496fa09a3c406be5e306b3596a5450b0a6
Mateusz Kiełbasa <>
2023-08-31 16:10:05 +0200

    Initial project commit

---
+++ include/stree.h
@@ -0,0 +1,10 @@
+#ifndef STREE_STREE_H
+#define STREE_STREE_H
+
+
+class stree {
+
+};
+
+
+#endif //STREE_STREE_H
---
+++ test/test1.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "stree.h"
+
+TEST(TestSuiteName, TestName) {
+    EXPECT_EQ(1, 1);
+}
